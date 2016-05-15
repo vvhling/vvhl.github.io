ce7313e684c4dc369211a63fce2b476e480012e2
741787064@qq.com <741787064@qq.com>
2016-05-15 10:42:14 +0800

    u

--- js/js.js
+++ js/js.js
@@ -1,10 +1,5 @@
 function changeNav(){
 	var home = document.getElementById("home");
-	var article = document.getElementById("article");
-	var myproject = document.getElementById("myproject");
-	var star = document.getElementById("star");
-	home.style.border-left="none";
-	home.style.border-top="none";
-	home.style.border-right="none"
-	home.style.border-bottom="solid 1px #000";
+	home.style.borderBottom = "solid 1px #000";
+	home.style.borderTop = "none";
 }
