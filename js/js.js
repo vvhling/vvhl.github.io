e34c386c79f46c69fc41489a8a1b5126b05e132e
741787064@qq.com <741787064@qq.com>
2016-05-15 12:58:27 +0800

    onmouseover

--- js/js.js
+++ js/js.js
@@ -1,4 +1,15 @@
 function changeNav(){
 	var home = document.getElementById("home");
 	home.style.borderBottom = "solid 1px #000";
+	home.style.borderLeft =  "none";
+	home.style.borderTop =  "none";
+	home.style.borderRight =  "none";
+}
+function changeNavOut(){
+	var home = document.getElementById("home");
+	home.style.borderBottom = "none";
+	home.style.borderLeft =  "solid 1px #000";
+	home.style.borderTop =  "solid 1px #000";
+	home.style.borderRight =  "solid 1px #000";
+}
 
