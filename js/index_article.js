6f23a778bf702ff4d61a1bc34287db357a503821
741787064@qq.com <741787064@qq.com>
2016-05-15 16:11:42 +0800

    9

---
+++ js/index_article.js
@@ -0,0 +1,17 @@
+/*导航部分*/
+function changeNav(){
+	var article = document.getElementById("article");
+	article.style.borderBottom = "solid 1px #000";
+	article.style.borderLeft =  "none";
+	article.style.borderTop =  "none";
+	article.style.borderRight =  "none";
+	article.style.width = "100%";
+}
+function changeNavOut(){
+	var article = document.getElementById("article");
+	article.style.borderBottom = "none";
+	article.style.borderLeft =  "solid 1px #000";
+	article.style.borderTop =  "solid 1px #000";
+	article.style.borderRight =  "solid 1px #000";
+	article.style.width = "98%";
+}
