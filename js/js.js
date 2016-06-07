17172f5f917c65f3c8da0eb96630e6a2522ffe11
vvhl <741787064@qq.com>
2016-06-07 18:34:52 +0800

    y

--- js/js.js
+++ js/js.js
@@ -1,18 +1,7 @@
 /*导航部分*/
-function changeNav(){
-	var home = document.getElementById("home");
-	home.style.borderBottom = "solid 1px #000";
-	home.style.borderLeft =  "none";
-	home.style.borderTop =  "none";
-	home.style.borderRight =  "none";
-	home.style.width = "100%";
-}
-function changeNavOut(){
-	var home = document.getElementById("home");
-	home.style.borderBottom = "none";
-	home.style.borderLeft =  "solid 1px #000";
-	home.style.borderTop =  "solid 1px #000";
-	home.style.borderRight =  "solid 1px #000";
-	home.style.width = "98%";
-}
+$(function(){
+	$(".article").bind("mouseover",function(){
+		$(".home").css("border-bottom":"solid 1px #000","border-top":"none","border-left":"none","border-right":"none");
+	});
+});
 
