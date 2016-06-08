9f01951c9c9846277d4a5364067d24837319f976
vvhl <741787064@qq.com>
2016-06-08 16:03:15 +0800

    u

--- js/js.js
+++ js/js.js
@@ -1,10 +1,10 @@
 /*导航部分*/
 $(function(){
     //导航菜单鼠标滑动效果  
-	$(".article,.myproject,.star").bind("mouseover",function(){
+	$(".myarticle,.myproject,.mystar").bind("mouseover",function(){
 		$(".home").css({"border-bottom":"1px solid #000","border-left":"none","border-top":"none","border-right":"none","width":"78px"});
 	});
-	$(".article,.myproject,.star").bind("mouseout",function(){
+	$(".myarticle,.myproject,.mystar").bind("mouseout",function(){
 		$(".home").css({"border-bottom":"none","border-left":"1px solid #000","border-top":"1px solid #000","border-right":"1px solid #000","width":"76px"});
 	});
 	//搜索框拉伸效果
