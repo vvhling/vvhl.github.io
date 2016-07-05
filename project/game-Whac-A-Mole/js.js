b64828615b6100e651fe3418a284aecda20be450
vvhl <741787064@qq.com>
2016-07-05 22:38:11 +0800

    1

--- project/game-Whac-A-Mole/js.js
+++ project/game-Whac-A-Mole/js.js
@@ -1,12 +1,15 @@
 $(function(){
 	function radom(){
 		var arr = $("li"); //获得一个数组
+        for(var i = 0,len = arr.length;i < len;i ++){ //遍历清空
+            arr[i].style.background = "#fff";
+        }
 		var l = arr.length; //获取到li的全部个数
 	    function radomNum(arr){
 	    	return arr[Math.floor(Math.random() * l)];
 	    }
 	    var radomList = radomNum(arr);  //返回的随机的Li元素
-	    var c = ["url('images/pic.png')"];
+	    var c = ["url('images/pic.png')","url('images/pic.png')"];
 	    var cl = c.length;
 	    function color(c){
 	    	return c[Math.floor(Math.random() * cl)];
@@ -16,6 +19,6 @@ $(function(){
 	}
 	var a = setInterval(function(){
     	radom();
-    },500)
+    },700)
 
 })
