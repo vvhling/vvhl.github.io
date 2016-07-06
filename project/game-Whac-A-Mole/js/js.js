113871219a7e6da19c65913dddc4ecb9099288c8
vvhl <741787064@qq.com>
2016-07-06 22:17:28 +0800

    1

--- project/game-Whac-A-Mole/js/js.js
+++ project/game-Whac-A-Mole/js/js.js
@@ -33,20 +33,45 @@
 $(function(){
     var randomA = function(){  //返回一个随机的a
         var a = $(".mouse");
-        for(var i = 0;i < a.length;i ++){
+        for(var i = 0;i < a.length;i ++){  //先遍历一遍a元素让它为初始状态
             $(a[i]).animate({height:"90px"});
         }
         var randomNum = Math.floor(Math.random() * a.length);
         return a[randomNum];
     };
-    setInterval(function(){
-        $(randomA()).animate({height:"150px"});
-    },2000)
+    var cli = function(){  //设置一个定时器
+        setInterval(function(){
+            $(randomA()).animate({height:"150px"});
+        },2000);
+    };
 
-    $("li").click(function(){
+    $("li").click(function(){  //点击li后地鼠缩回洞里
         $(this).find("a").animate({height:"90px"});
     });
 
+    
+
+
+    var score = function(){  //点击事件执行的次数
+        var c = 0;
+        if(cli()){
+            c ++;
+        }
+        return c;
+    }
+
+    $(".num").html(score());
+    function s(){
+        var sec = 30;
+        for(var i = 0;i < sec; i ++){
+            sec --;
+        }
+        return sec;
+    }
+
+    setTimeout(function(){
+        $(".time").html(s());
+    },1000)
 });
 
 
