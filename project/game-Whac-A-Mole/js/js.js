cb74d88974ab4b8840a9921f7a5e3fa8eb489376
vvhl <741787064@qq.com>
2016-07-06 23:27:43 +0800

    1

--- project/game-Whac-A-Mole/js/js.js
+++ project/game-Whac-A-Mole/js/js.js
@@ -1,35 +1,3 @@
-//$(function(){
-//	function radom(){
-//		var arr = $("li"); //获得一个数组
-//         for(var i = 0,len = arr.length;i < len;i ++){ //遍历清空
-//             arr[i].style.background = "#fff";
-//         }
-//		var l = arr.length; //获取到li的全部个数
-//	    function radomNum(arr){
-//	    	return arr[Math.floor(Math.random() * l)];
-//	    }
-//	    var radomList = radomNum(arr);  //返回的随机的Li元素
-//	    var c = ["url('images/mouse.png')"];
-//	    var cl = c.length;
-//	    function color(c){
-//	    	return c[Math.floor(Math.random() * cl)];
-//	    }
-//	    var radomColor = color(c); //返回的随机颜色
-//	    radomList.style.background = radomColor;
-//	}
-//	var a = setInterval(function(){  //创建一个定时器
-//     	radom();
-//     },1000);
-//
-//     var clickTimes = function(){
-//         $(".box").click(function(){
-//             $(this).css("background","#fff");
-//         });
-//         return x;
-//     };
-//     clickTimes();
-//});
-
 $(function(){
     var randomA = function(){  //返回一个随机的a
         var a = $(".mouse");
@@ -39,39 +7,22 @@ $(function(){
         var randomNum = Math.floor(Math.random() * a.length);
         return a[randomNum];
     };
-    var cli = function(){  //设置一个定时器
-        setInterval(function(){
-            $(randomA()).animate({height:"150px"});
-        },2000);
+
+    setInterval(function(){  //设置一个定时器
+        $(randomA()).animate({height:"150px"});
+    },2000);
+
+    var c = 0;
+    var score = function(){  //点击事件执行的次数
+        $("li").click(function(){  //点击li后地鼠缩回洞里
+            c++;
+            $(this).find("a").animate({height:"90px"});
+        });
+        return c;
     };
 
-    $("li").click(function(){  //点击li后地鼠缩回洞里
-        $(this).find("a").animate({height:"90px"});
-    });
-
-    
-
-
-    var score = function(){  //点击事件执行的次数
-        var c = 0;
-        if(cli()){
-            c ++;
-        }
-        return c;
-    }
-
     $(".num").html(score());
-    function s(){
-        var sec = 30;
-        for(var i = 0;i < sec; i ++){
-            sec --;
-        }
-        return sec;
-    }
 
-    setTimeout(function(){
-        $(".time").html(s());
-    },1000)
 });
 
 
