c972b9057b2633cabe48dc757d7edb6215d74ea2
vvhling <vvhling@gmail.com>
2016-07-07 14:52:55 +0800

    Update js.js

--- project/game-Whac-A-Mole/js/js.js
+++ project/game-Whac-A-Mole/js/js.js
@@ -1,29 +1,27 @@
-$(function(){
-    var randomA = function(){  //返回一个随机的a
-        var a = $(".mouse");
-        for(var i = 0;i < a.length;i ++){  //先遍历一遍a元素让它为初始状态
-            $(a[i]).animate({height:"90px"});
-        }
-        var randomNum = Math.floor(Math.random() * a.length);
-        return a[randomNum];
-    };
+var randomA = function(){  //返回一个随机的a
+    var a = $(".mouse");
+    for(var i = 0;i < a.length;i ++){  //先遍历一遍a元素让它为初始状态
+        $(a[i]).animate({height:"90px"});
+    }
+    var randomNum = Math.floor(Math.random() * a.length);
+    return a[randomNum];
+};
 
-    setInterval(function(){  //设置一个定时器
-        $(randomA()).animate({height:"150px"});
-    },2000);
+setInterval(function(){  //设置一个定时器
+    $(randomA()).animate({height:"150px"});
+},2000);
 
+function clickMouse(){  //老鼠点击事件
     var c = 0;
-    var score = function(){  //点击事件执行的次数
-        $("li").click(function(){  //点击li后地鼠缩回洞里
-            c++;
-            $(this).find("a").animate({height:"90px"});
-        });
-        return c;
-    };
-
-    $(".num").html(score());
-
-});
+    $("li").click(function(){
+        if($(this).find("a").height() == 150){
+            c ++;
+            $(".num").html(c);
+        }
+        $(this).find("a").animate({height:"90px"});
+    });
+}
+clickMouse();
 
 
 
