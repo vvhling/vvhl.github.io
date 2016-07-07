2f4a71a091157f5b3ecb6a0a2c7f2fab41dfd7d1
vvhl <741787064@qq.com>
2016-07-07 22:35:52 +0800

    111

--- project/game-Whac-A-Mole/js/js.js
+++ project/game-Whac-A-Mole/js/js.js
@@ -1,31 +1,34 @@
 $(function(){
 //随机事件
-var randomA = function(){  //返回一个随机的a
+function random(){
+    var arrColor = [
+        "images/mouse.png",
+        "images/mouse-black.png",
+        "images/mouse-red.png",
+        "images/mouse-green.png",
+        "images/mouse-blue.png"
+    ];
+    var randomColor = function(){  //返回一个随机颜色
+        var randomClo = Math.floor(Math.random() * arrColor.length);
+        return arrColor[randomClo];
+    };
     var a = $(".mouse");
     for(var i = 0;i < a.length;i ++){  //先遍历一遍a元素让它为初始状态
         $(a[i]).animate({height:"90px"});
     }
-    var randomNum = Math.floor(Math.random() * a.length);
-    return a[randomNum];
-};
-
-var randomColor = function(){  //返回一个随机颜色的老鼠
-    var arrColor = [
-        "images/mouse-red.png",
-        "images/mouse-green.png",
-        "images/mouse-blue.png",
-        "images/mouse-black.png"
-    ];
-    var randomClo = Math.floor(Math.random() * arrColor.length);
-    return arrColor[randomClo];
-};
-    console.log(randomColor());
-
-$(randomA()).children("img").src = randomColor();
-
-setInterval(function(){  //设置一个定时器
+    var randomA = function(){  //返回一个随机的a
+        var randomNum = Math.floor(Math.random() * a.length);
+        return a[randomNum];
+    };
     $(randomA()).animate({height:"150px"});
-},2000);
+    if($(randomA()).height != "150px"){
+        $(randomA()).children("img").attr("src",randomColor());
+    }
+
+}
+setInterval(function(){  //设置一个定时器
+    random();
+},1000);
 
 function clickMouse(){  //老鼠点击事件
     var c = 0;
